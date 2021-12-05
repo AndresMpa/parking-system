cc4d371b6378f90719ea2b61eb556a70702c1a23
AndresMpa <andresprieto2703@gmail.com>
2021-12-05 18:20:32 -0500

    Fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add --all
 git commit -m 'deploy'
-git push -f git@github.com:AndresMpa/parking-system.git master:gh-pages
+git push -f git@github.com:AndresMpa/parking-system.git main:gh-pages
 cd -
